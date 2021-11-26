a8892f0d08f50344b4189b269b776904894662aa
Rowan Molony <rowan.molony@mainstreamrp.com>
2021-11-26 16:19:44 +0000

    Add first unit test and url mapping using a dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,2 +1,6 @@
-def test_bath_maths():
-    assert 1 + 1 == 3
+from django.urls import resolve
+from lists.views import home_page
+
+def test_root_url_resolves_to_home_page_view():
+    found = resolve("/")
+    assert found.func == home_page
