deb98f9aa6fb688728512f298f2b25d4f600a765
Rowan Molony <rowan.molony@mainstreamrp.com>
2021-11-26 16:39:22 +0000

    Check can add a to-do list item in functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,9 @@
 import socket
+import time
 
 import pytest
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
@@ -32,19 +34,31 @@ def test_can_start_a_list_and_retrieve_it_later(
 
     # She notices the page title and header mention to-do lists
     assert "To-Do" in browser.title
-    pytest.fail("Finish the test!")
-
+    header_text = browser.find_element_by_tag_name("h1").text
+    assert "To-Do" in header_text
+    
     # She is invited to enter a to-do item straight away
+    inputbox = browser.find_element_by_id("id_new_name")
+    assert inputbox.get_attribute("placeholder") == "Enter a to-do item" 
 
     # She types "Buy peacock feathers" into a text box (Edith's hobby
     # is tying fly-fishing lures)
+    inputbox.send_keys("Buy peacock feathers")
 
     # When she hits enter, the page updates, and now the page lists
     # "1: Buy peacock feathers" as an item in a to-do list
+    inputbox.send_keys(Keys.ENTER)
+    time.sleep(1)
+
+    table = browser.find_element_by_id("id_list_table")
+    rows = table.find_elements_by_tag_name("tr")
+    assert any(row.text == "1: Buy peacock feathers" for row in rows)
+
 
     # There is still a text box inviting her to add another item. She
     # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-
+    pytest.fail("Finish the test!")
+    
     # The page updates again, and now shows both items on her list
 
     # Edith wonders whether the site will remember her list. Then she sees
