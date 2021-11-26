a4492f2dd22ca2214a0bc7f4341c6f29f01a02d0
Rowan Molony <rowan.molony@mainstreamrp.com>
2021-11-26 16:30:28 +0000

    Return minimal HTML from home page view

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,17 @@
+from django.http import HttpRequest, response
 from django.urls import resolve
+
 from lists.views import home_page
 
-def test_root_url_resolves_to_home_page_view():
+def test_root_url_resolves_to_home_page_view() -> None:
     found = resolve("/")
     assert found.func == home_page
+
+
+def test_home_page_returns_correct_html() -> None:
+    request = HttpRequest()
+    response = home_page(request)
+    html = response.content.decode("utf8")
+    assert html.startswith("<html>")
+    assert "<title>To-Do lists</title>" in html
+    assert html.endswith("</html>")
--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpRequest
+from django.http import HttpResponse
 from django.shortcuts import render
 
-def home_page():
-    pass
+def home_page(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("<html><title>To-Do lists</title></html>")
