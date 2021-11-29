1a9dd77ba4a42ed8fe4b032f4f66ddf375b7682f
Rowan Molony <rowan.molony@mainstreamrp.com>
2021-11-29 15:07:07 +0000

    Add model for list Item & make migrations

--- lists/tests.py
+++ lists/tests.py
@@ -2,8 +2,12 @@ from django.http import HttpRequest, response
 from django.test.client import Client
 from django.urls import resolve
 
-from lists.views import home_page
+import pytest
 from pytest_django.asserts import assertTemplateUsed
+
+from lists.models import Item
+from lists.views import home_page
+
 
 def test_root_url_resolves_to_home_page_view() -> None:
     found = resolve("/")
@@ -28,3 +32,22 @@ def test_can_save_a_POST_request(client: Client) -> None:
     response = client.post("/", data={"item_text": "A new list item"})
     assert "A new list item" in response.content.decode()
     assertTemplateUsed(response, "home.html")
+
+
+@pytest.mark.django_db
+def test_saving_and_retrieving_items(client: Client) -> None:
+    first_item = Item()
+    first_item.text = "The first (ever) list item"
+    first_item.save()
+
+    second_item = Item()
+    second_item.text = "Item the second"
+    second_item.save()
+
+    saved_items = Item.objects.all()
+    assert saved_items.count() == 2
+
+    first_saved_item = saved_items[0]
+    second_saved_item = saved_items[1]
+    assert first_saved_item.text == "The first (ever) list item"
+    assert second_saved_item.text == "Item the second"
