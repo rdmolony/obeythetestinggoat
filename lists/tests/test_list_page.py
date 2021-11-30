ff1bbd6eeb65f10d85bd8eac533579e0ad19b2e5
Rowan Molony <rowan.molony@mainstreamrp.com>
2021-11-30 16:14:34 +0000

    Save each session list to a unique user id
    Redirect adding to the to-do list to {list_id}/add_item
    which only adds to the specific table related to the list
    at hand.  This means that different sessions will have
    different lists

--- lists/tests/test_list_page.py
+++ lists/tests/test_list_page.py
@@ -9,20 +9,27 @@ from lists.models import List
 
 
 @pytest.mark.django_db
-def test_displays_all_items(client: Client) -> None:
-    list_ = List.objects.create()
-    Item.objects.create(text="itemey 1", list=list_)
-    Item.objects.create(text="itemey 2", list=list_)
+def test_displays_only_items_for_that_list(client: Client) -> None:
+    correct_list = List.objects.create()
+    Item.objects.create(text="itemey 1", list=correct_list)
+    Item.objects.create(text="itemey 2", list=correct_list)
 
-    response = client.get("/lists/the-only-list-in-the-world/")
+    other_list = List.objects.create()
+    Item.objects.create(text="other list item 1", list=other_list)
+    Item.objects.create(text="other list item 2", list=other_list)
+
+    response = client.get(f"/lists/{correct_list.id}/")
 
     assert "itemey 1" in response.content.decode()
     assert "itemey 2" in response.content.decode()
+    assert "other list item 1" not in response.content.decode()
+    assert "other list item 2" not in response.content.decode()
 
 
 @pytest.mark.django_db
 def test_uses_list_template(client: Client) -> None:
-    response = client.get("/lists/the-only-list-in-the-world/")
+    list_ = List.objects.create()
+    response = client.get(f"/lists/{list_.id}/")
     assertTemplateUsed(response, "list.html")
 
 
@@ -38,5 +45,42 @@ def test_can_save_a_POST_request(client: Client) -> None:
 @pytest.mark.django_db
 def test_redirects_after_POST(client: Client) -> None:
     response = client.post("/lists/new", data={"item_text": "A new list item"})
+    new_list = List.objects.first()
+    assertRedirects(response, f"/lists/{new_list.id}/")
 
-    assertRedirects(response, "/lists/the-only-list-in-the-world/")
+
+@pytest.mark.django_db
+def test_can_save_a_POST_request_to_an_existing_list(client: Client) -> None:
+    other_list = List.objects.create()
+    correct_list = List.objects.create()
+
+    client.post(
+        f"/lists/{correct_list.id}/add_item",
+        data={"item_text": "A new item for an existing list"},
+    )
+
+    assert Item.objects.count() == 1
+    new_item = Item.objects.first()
+    assert new_item.text == "A new item for an existing list"
+    assert new_item.list == correct_list
+
+
+@pytest.mark.django_db
+def test_redirects_to_list_view(client: Client) -> None:
+    other_list = List.objects.create()
+    correct_list = List.objects.create()
+
+    response = client.post(
+        f"/lists/{correct_list.id}/add_item",
+        data={"item_text": "A new item for an existing list"},
+    )
+
+    assertRedirects(response, f"/lists/{correct_list.id}/")
+
+
+@pytest.mark.django_db
+def test_passes_correct_list_to_template(client: Client) -> None:
+    other_list = List.objects.create()
+    correct_list = List.objects.create()
+    response = client.get(f"/lists/{correct_list.id}/")
+    assert response.context["list"] == correct_list
--- lists/views.py
+++ lists/views.py
@@ -11,12 +11,19 @@ def home_page(request: HttpRequest) -> HttpResponse:
     return render(request, "home.html")
 
 
-def view_list(request: HttpRequest) -> HttpResponse:
-    items = Item.objects.all()
-    return render(request, "list.html", {"items": items})
+def view_list(request: HttpRequest, list_id: str) -> HttpResponse:
+    list_ = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=list_)
+    return render(request, "list.html", {"list": list_})
 
 
 def new_list(request: HttpRequest) -> HttpResponse:
     list_ = List.objects.create()
     Item.objects.create(text=request.POST["item_text"], list=list_)
-    return redirect("/lists/the-only-list-in-the-world/")
+    return redirect(f"/lists/{list_.id}/")
+
+
+def add_item(request: HttpRequest, list_id: str) -> HttpResponse:
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST["item_text"], list=list_)
+    return redirect(f"/lists/{list_.id}/")
